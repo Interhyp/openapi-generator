f8b047895d8c9cccf346e9a6feba03b8fcad135a
Plushnikov, Michail <michail.plushnikov@interhyp.de>
2023-06-12 15:09:35 +0200

    RELTEC-11694: created new client generator type: "go-autumrest" RELTEC-11695: change Error to IError interface name RELTEC-11667: added new client functions RELTEC-11417: parse only 100/200/300 responses as success responses RELTEC-11372: reworked api-client generation RELTEC-11372: api refactored for errorDto/response support RELTEC-11349: document release process in shell script RELTEC-11349: support additional property forceLargeNumbers=true to force int64,float64 RELTEC-11336: support "allowReserved" property RELTEC-11265: escape path param RELTEC-11265: support array query params RELTEC-11265: wrap http err with custom struct RELTEC-11265: basic support for multipart-upload, some tweaks RELTEC-11283: fix constant "time" import issue, support enum modell types RELTEC-11228: remove github actions to prevent side effects RELTEC-11265: added writeOnly Flag support RELTEC-11228: prefix interhyp specific generated files as 'generated_', RELTEC-11228: added Request-Execute-Style-API RELTEC-11228: configure configuredHeaderManipulator RELTEC-11228: added api generation for aurest

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultGenerator.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultGenerator.java
@@ -83,6 +83,7 @@ public class DefaultGenerator implements Generator {
     protected CodegenIgnoreProcessor ignoreProcessor;
     private Boolean generateApis = null;
     private Boolean generateModels = null;
+    private Boolean generateClient = null;
     private Boolean generateRecursiveDependentModels = null;
     private Boolean generateSupportingFiles = null;
     private Boolean generateWebhooks = null;
@@ -98,7 +99,7 @@ public class DefaultGenerator implements Generator {
     /**
      * -- GETTER --
      *  Retrieves an instance to the configured template processor, available after user-defined options are
-     *  applied via 
+     *  applied via
      * .
      *
      * @return A configured {@link TemplateProcessor}, or null.
@@ -207,6 +208,7 @@ public class DefaultGenerator implements Generator {
         // NOTE: Boolean.TRUE is required below rather than `true` because of JVM boxing constraints and type inference.
         generateApis = GlobalSettings.getProperty(CodegenConstants.APIS) != null ? Boolean.TRUE : getGeneratorPropertyDefaultSwitch(CodegenConstants.APIS, null);
         generateModels = GlobalSettings.getProperty(CodegenConstants.MODELS) != null ? Boolean.TRUE : getGeneratorPropertyDefaultSwitch(CodegenConstants.MODELS, null);
+        generateClient = GlobalSettings.getProperty(CodegenConstants.GENERATE_CLIENT) != null ? Boolean.TRUE : getGeneratorPropertyDefaultSwitch(CodegenConstants.GENERATE_CLIENT, null);
         generateSupportingFiles = GlobalSettings.getProperty(CodegenConstants.SUPPORTING_FILES) != null ? Boolean.TRUE : getGeneratorPropertyDefaultSwitch(CodegenConstants.SUPPORTING_FILES, null);
         generateWebhooks = GlobalSettings.getProperty(CodegenConstants.WEBHOOKS) != null ? Boolean.TRUE : getGeneratorPropertyDefaultSwitch(CodegenConstants.WEBHOOKS, null);
 
@@ -244,6 +246,7 @@ public class DefaultGenerator implements Generator {
 
         config.additionalProperties().put(CodegenConstants.GENERATE_APIS, generateApis);
         config.additionalProperties().put(CodegenConstants.GENERATE_MODELS, generateModels);
+        config.additionalProperties().put(CodegenConstants.GENERATE_CLIENT, generateClient);
         config.additionalProperties().put(CodegenConstants.GENERATE_WEBHOOKS, generateWebhooks);
         config.additionalProperties().put(CodegenConstants.GENERATE_RECURSIVE_DEPENDENT_MODELS, generateRecursiveDependentModels);
 
@@ -554,7 +557,7 @@ public class DefaultGenerator implements Generator {
                 }
             }
         }
-        
+
         // generate files based on processed models
         for (String modelName : allProcessedModels.keySet()) {
             ModelsMap models = allProcessedModels.get(modelName);
